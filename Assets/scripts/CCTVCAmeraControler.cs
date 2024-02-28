9fe632c02d4694ba2b543e0ec92babc02145c264
Mateusz Kurek <mateusz.kurek4@teb.edu.pl>
2024-02-28 13:48:25 +0100

    sdsd

--- Assets/scripts/CCTVCAmeraControler.cs
+++ Assets/scripts/CCTVCAmeraControler.cs
@@ -7,7 +7,7 @@ public class CCTVCAmeraControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sd
     }
 
     // Update is called once per frame

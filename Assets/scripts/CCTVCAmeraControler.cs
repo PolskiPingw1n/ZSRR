2b2ebcf1a0860925b42e1d02ab129ef99bacf3bc
Mateusz Kurek <mateusz.kurek4@teb.edu.pl>
2024-03-06 14:47:37 +0100

    boks

--- Assets/scripts/CCTVCAmeraControler.cs
+++ Assets/scripts/CCTVCAmeraControler.cs
@@ -27,9 +27,7 @@ public class CCTVCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // mdyfikujemy rotacjê obiektu za pomoc¹ funkcji PingPong, która generuje wartoœci
-        // oscyluj¹ce pomiêdzy 0 a 9, a nastêpnie mno¿ymy to przez 10 ¿eby uzyskaæ szybszy ruch
-        // i na koniec odejmujemy od wartoœci otrzymanej 45 stopni aby uzyskaæ ruch w zakresie -45 do 45
+     
         transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.PingPong(Time.time, 9) * 10 - 45, 0));
 
         CheckIfPlayerVisible();
@@ -37,7 +35,7 @@ public class CCTVCameraController : MonoBehaviour
 
     void CheckIfPlayerVisible()
     {
-        Debug.DrawRay(cameraLens.position, cameraLens.TransformDirection(Vector3.down) * 100, Color.yellow);
+       // Debug.DrawRay(cameraLens.position, cameraLens.TransformDirection(Vector3.down) * 100, Color.yellow);
     }
 
 }
